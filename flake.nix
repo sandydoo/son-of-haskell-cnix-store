6a39a0e31e820be7a9338120da7674759bf77a40
Sander <hey@sandydoo.me>
2023-09-12 16:23:11 +0300

    Add formatting and pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix.url = "github:tweag/nix/nix-c-bindings";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = inputs@{ self, nix, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nix, nixpkgs, flake-utils, pre-commit-hooks, ... }:
     let systems = builtins.attrNames nix.packages;
     in flake-utils.lib.eachSystem systems (system:
       let
@@ -20,7 +21,18 @@
             cabal-install
             ghc
             haskell-language-server
+            ormolu
           ];
+        };
+
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              ormolu.enable = true;
+            };
+          };
         };
       });
 }
