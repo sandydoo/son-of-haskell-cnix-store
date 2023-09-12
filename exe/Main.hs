6c5727f7a06f2593503fef8bc5797250b6752308
Sander <hey@sandydoo.me>
2023-09-12 14:33:53 +0300

    Add a basic demo

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,29 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Control.Exception
 import Foreign.C
 import Foreign.Marshal.Alloc (allocaBytes)
 import Foreign.Ptr
-import CNix
+import qualified System.Nix.C.Store as CNix
+import qualified System.Nix.C.Util as CNix
 
 main :: IO ()
 main = do
   putStrLn "Nix C API interop"
   putStrLn "Initializing libstore"
-  result <- initStore
+  result <- CNix.nix_libstore_init
   putStrLn $ "Initialized store. Return code: " ++ show result
 
-  putStrLn "Opening store"
+  let uri = "daemon"
+  putStrLn $ "Opening store: " ++ uri
 
-  uri <- newCString "/nix/store"
-  bracket (openStore uri nullPtr) unrefStore tryInteract
+  bracket (openStore uri) releaseStore tryInteract
 
-tryInteract :: Store -> IO ()
-tryInteract store = do
+tryInteract :: Either String CNix.Store -> IO ()
+tryInteract (Left err) = putStrLn err
+tryInteract (Right store) = do
   putStrLn "Fetching version"
 
-  clientVersion <- peekCString =<< getVersionStatic
-  mdaemonVersion <- getVersion' store
+  clientVersion <- peekCString =<< CNix.nix_version_get
+  mdaemonVersion <- getVersion store
 
   case mdaemonVersion of
     Just daemonVersion ->
@@ -32,12 +35,66 @@ tryInteract store = do
 
   putStrLn $ "Library version: " ++ clientVersion
 
-getVersion' :: Store -> IO (Maybe String)
-getVersion' store = do
+  let notInStorePath = "/not/in/store/path"
+  checkIfValidPath store notInStorePath
+
+  let invalidPath = "/nix/store/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-stuff"
+  checkIfValidPath store invalidPath
+
+  let validPath = "/nix/store/0yy1jjpcimdlw2kp9kd2nmhnpss0zr19-cachix-1.6"
+  checkIfValidPath store validPath
+
+checkIfValidPath :: CNix.Store -> String -> IO ()
+checkIfValidPath store path = do
+  putStrLn $ "Checking store path: " ++ path
+  cdata <- CNix.nix_c_context_create
+  storePath <- CNix.nix_store_parse_path cdata store =<< newCString path
+  if (CNix.unStorePath storePath) == nullPtr then do
+    getErrMsg cdata
+  else do
+    isValid <- CNix.nix_store_is_valid_path cdata store storePath
+    putStrLn $ path ++ " is " ++ if isValid == 1 then "valid" else "invalid"
+
+  CNix.nix_c_context_free cdata
+
+getVersion :: CNix.Store -> IO (Maybe String)
+getVersion store = do
   let bufferSize = 1024
   allocaBytes bufferSize $ \ptr -> do
-    result <- getVersion nullPtr store ptr (fromIntegral bufferSize)
+    result <- CNix.nix_store_get_version nullPtr store ptr (fromIntegral bufferSize)
 
     if result == 0
       then Just <$> peekCString ptr
       else return Nothing
+
+openStore :: String -> IO (Either String CNix.Store)
+openStore uri = do
+  curi <- newCString uri
+  cdata <- CNix.nix_c_context_create
+  mstore <- CNix.nix_store_open cdata curi nullPtr
+
+  code <- CNix.nix_err_code cdata
+  putStrLn $ "Error code: " ++ show code
+
+  if code /= 0
+  then do
+    getErrMsg cdata
+    CNix.nix_c_context_free cdata
+    return (Left "Failed to open store")
+  else do
+    CNix.nix_c_context_free cdata
+    return (Right mstore)
+
+
+releaseStore :: Either String CNix.Store -> IO ()
+releaseStore (Left _) = return ()
+releaseStore (Right store) = CNix.nix_store_unref store
+
+getErrMsg :: Ptr CNix.Context -> IO ()
+getErrMsg cdata = do
+  let bufferSize = 1024
+  allocaBytes bufferSize $ \ptr -> do
+    _ <- CNix.nix_err_name nullPtr cdata ptr (fromIntegral bufferSize) 
+    errMsg <- peekCString =<< CNix.nix_err_msg nullPtr cdata nullPtr
+    errName <- peekCString ptr
+    putStrLn $ errName ++ ": " ++ errMsg
